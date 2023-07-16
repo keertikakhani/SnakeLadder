3c62e76615289258ea8b1911207e29e34efb68a3
Your Name <you@example.com>
2023-07-16 22:04:50 +0530

    player is checking for an option and those are no play, snake and ladder

--- SnakeLadder/Snake.cs
+++ SnakeLadder/Snake.cs
@@ -11,12 +11,36 @@ namespace SnakeLadder
         public static void Ladder()
         {
             int dice;
+            int option;
+            int position = 0;
             Console.WriteLine("Snake and ladder game with single player");
             Console.WriteLine("player is at 0 position");
+            Console.WriteLine("player is rolling the dice");
+
             Random random = new Random();
             dice = random.Next(1, 7);
-            Console.WriteLine("Player is rolling the dice");
             Console.WriteLine("dice value:" + dice);
+            Random random1 = new Random();
+            option = random1.Next(0, 3);
+
+            if (option == 0)
+            {
+                Console.WriteLine("\noption is No play");
+                position = position + 0;
+                Console.WriteLine("position : " + position);
+            }
+            else if (option == 1)
+            {
+                Console.WriteLine("\noption is snake");
+                position = position - dice;
+                Console.WriteLine("position : " + position);
+            }
+            else
+            {
+                Console.WriteLine("\noption is ladder");
+                position = position + dice;
+                Console.WriteLine("position : " + position);
+            }
         }
     }
 }
