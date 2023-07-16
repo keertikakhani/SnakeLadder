b6457152725f9a73140010a8be48bc120721d3a3
Your Name <you@example.com>
2023-07-16 21:47:08 +0530

    Adding welcome message

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+
+            Console.ReadLine();
+        }
+    }
+}
