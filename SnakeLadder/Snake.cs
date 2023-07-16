ce0318cc1bc89170b729e32137c0f973298ca8fd
Your Name <you@example.com>
2023-07-16 21:53:39 +0530

    Player is rolling the dice

--- SnakeLadder/Snake.cs
+++ SnakeLadder/Snake.cs
@@ -10,8 +10,13 @@ namespace SnakeLadder
     {
         public static void Ladder()
         {
+            int dice;
             Console.WriteLine("Snake and ladder game with single player");
             Console.WriteLine("player is at 0 position");
+            Random random = new Random();
+            dice = random.Next(1, 7);
+            Console.WriteLine("Player is rolling the dice");
+            Console.WriteLine("dice value:" + dice);
         }
     }
 }
